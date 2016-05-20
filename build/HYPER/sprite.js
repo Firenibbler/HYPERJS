c0eef213350e7fb573130d8957f15320dce6b1ce
jtenner <tenner.joshua@gmail.com>
2016-05-20 15:38:51 -0400

    Update sprite.js
    Changes Made:
    - Attach functions to prototype instead
    - Create noOp property for default empty function values
    - Degrees parsing using while loops and detect finite values in case of bad user input

--- build/HYPER/sprite.js
+++ build/HYPER/sprite.js
@@ -1,30 +1,15 @@
-(function () {
+(function() {
     "use strict";
     // SPRITE
-
+    var noOp = function() {};
     /**
      * Description Used to create the basic game object, useful for buttons, objects, and charicters.
      */
-    HYPER.Sprite = function (e) {
-        
+    HYPER.Sprite = function Sprite(e) {
+
         this.children = [];
-
-        this.addChild = function (child) {
-            this.children.push(child);
-        };
-        this.removeChild = function (child) {
-            var i = this.children.indexOf(child);
-            if (i !== -1) {
-                this.children.splice(i, 1);
-            }
-            return this;
-        };
-        this.removeAllChildren = function (child) {
-            this.children.splice(0, this.children.length);
-            return this;
-        };
-
         this._ShapeType = "rect";
+
         this._ID = Math.random() * Math.random();
         this.x = e.x || 0; //coalesce to 0 for falsy values 0, NaN, null, undefined
         this.y = e.y || 0;
@@ -36,7 +21,7 @@
         e.rotation = e.rotation || {};
         this.rotation.y = (typeof e.rotation.y === 'undefined') ? this.height / 2 : e.rotation.y;
         this.rotation.x = (typeof e.rotation.x === 'undefined') ? this.width / 2 : e.rotation.x;
-        this.visable = (typeof e.visable === 'undefined') ? true : e.visable;
+        this.visible = (typeof e.visible === 'undefined') ? true : e.visible;
         this.style = new HYPER.Graphics.Style(e.style);
         e.size = e.size || {};
         this.size = e.size;
@@ -46,12 +31,12 @@
         this.size.width = (typeof e.size.width === 'undefined') ? this.width : e.size.width;
         this.size.height = (typeof e.size.height === 'undefined') ? this.height : e.size.height;
 
-        this.onclick = function () {};
-        this.onhold = function () {};
-        this.onup = function () {};
-        this.ondown = function () {};
-        this.onhover = function () {};
-        this.ondblclick = function () {};
+        this.onclick = noOp;
+        this.onhold = noOp;
+        this.onup = noOp;
+        this.ondown = noOp;
+        this.onhover = noOp;
+        this.ondblclick = noOp;
 
         this.events = {};
         this.events.onclick = [];
@@ -61,138 +46,131 @@
         this.events.onhover = [];
         this.events.ondblclick = [];
 
-        this.addEventListener = function (event, callback) {
-            if (typeof this.events[event] === "undefined") {
-                console.error(event + " is not a real event");
-            } else {
-                callback._ID = Math.random() * Math.random();
-                this.events[event].push(callback);
-                return callback._ID;
-            };
-        };
-        this.removeEventListener = function (event, callback) {
-            if (typeof this.events[event] === "undefined") {
-                console.error(event + " is not a real event");
-            } else {
-                for (var i = 0; i < this.events[event].length; i++) {
-                    if (this.events[event]._ID == callback || this.events[event]._ID == callback._ID) {
-                        this.events[event].splice(i, 1);
-                    };
-                };
-            };
-        };
-        this.removeAllEventListeners = function (event) {
-            if (typeof this.events[event] === "undefined") {
-                console.error(event + " is not a real event");
-            } else {
-                this.events[event] = [];
-            };
-        }
+
         this.physics = {};
-        this.physics.update = function () {};
+        this.physics.update = noOp;
 
-        this.render = e.render || function () {};
-        this._render = function (o) {
-            if (this.visable == true) {
+        this.render = e.render || noOp;
 
-                for (var i = 0; i < this.children.length; i++) {
+        this.update = e.update || noOp;
 
+    };
+    HYPER.Sprite.prototype.addChild = function addChild(child) {
+        this.children.push(child);
+        return this;
+    };
+    HYPER.Sprite.prototype.removeChild = function removeChild(child) {
+        var i = this.children.indexOf(child);
+        if (i !== -1) {
+            this.children.splice(i, 1);
+        }
+        return this;
+    };
+    HYPER.Sprite.prototype.removeAllChildren = function removeAllChildren(child) {
+        this.children.splice(0, this.children.length);
+        return this;
+    };
 
-
-                    this.children[i]._render({
-                        camera: {
-                            x: this.x + o.camera.x,
-                            y: this.y + o.camera.y,
-                            width: this.width,
-                            height: this.height,
-                        },
-                        ctx: o.ctx,
-                        offsetLeft: o.offsetLeft,
-                        offsetTop: o.offsetTop,
-                        view: o.view,
-                        alpha: this.alpha,
-                    });
-
-
-                };
-            }
-            this.render(o);
-        };
-        this.update = e.update || function () {};
-        this._update = function (o) {
-
+    HYPER.Sprite.prototype._render = function _render(o) {
+        if (this.visible) {
             for (var i = 0; i < this.children.length; i++) {
-                this.children[i]._update({
+                this.children[i]._render({
                     camera: {
                         x: this.x + o.camera.x,
                         y: this.y + o.camera.y,
                         width: this.width,
                         height: this.height,
                     },
+                    ctx: o.ctx,
                     offsetLeft: o.offsetLeft,
                     offsetTop: o.offsetTop,
                     view: o.view,
+                    alpha: this.alpha,
                 });
-            };
-
-            this.update(o);
-            this.physics.update();
-
-            if (this.size.autoAdjust == true) {
-                this.size.width = this.width;
-                this.size.height = this.height;
             }
+        }
+        this.render(o);
+    };
 
-            if (this.degrees >= 360) {
-                this.degrees -= 360;
-            }
+    HYPER.Sprite.prototype._update = function _update(o) {
 
-            for (var i = 0; i < 10; i++) {
-                var pointer = HYPER.Input.Pointer.getPointer(i);
-                this._pointerX = ((pointer.x - o.offsetLeft) * (o.camera.width / o.view.width)) - o.camera.x;
-                this._pointerY = ((pointer.y - o.offsetTop) * (o.camera.height / o.view.height)) - o.camera.y;
-                if (this._pointerX >= this.x + this.size.x &&
-                    this._pointerX <= this.x + this.size.width &&
-                    this._pointerY >= this.y + this.size.y &&
-                    this._pointerY <= this.y + this.size.height) {
-                    this._InputInfo = {
-                        x: this._pointerX,
-                        y: this._pointerY
-                    }
-                    for (var b = 0; b < this.events.onhover.length; b++) {
-                        this.events.onhover[b](this._InputInfo);
-                        this.onhover(this._InputInfo);
-                    }
-                    if (pointer.down) {
-                        for (var b = 0; b < this.events.ondown.length; b++) {
-                            this.events.ondown[b](this._InputInfo);
-                            this.ondown(this._InputInfo);
-                        }
-                    }
-                    if (pointer.up) {
-                        for (var b = 0; b < this.events.onup.length; b++) {
-                            this.events.onup[b](this._InputInfo);
-                            this.onup(this._InputInfo);
-                        }
-                        for (var b = 0; b < this.events.onclick.length; b++) {
-                            this.events.onclick[b](this._InputInfo);
-                            this.onclick(this._InputInfo);
-                        }
-                    }
-                    if (pointer.hold) {
-                        for (var b = 0; b < this.events.onhold.length; b++) {
-                            this.events.onhold[b](this._InputInfo);
-                            this.onhold(this._InputInfo);
-                        }
-                    }
-                    if (pointer.dblclick) {
-                        for (var b = 0; b < this.events.ondblclick.length; b++) {
-                            this.events.ondblclick[b](this._InputInfo);
-                            this.ondblclick(this._InputInfo);
-                        }
-                    }
+      for (var i = 0; i < this.children.length; i++) {
+          this.children[i]._update({
+              camera: {
+                  x: this.x + o.camera.x,
+                  y: this.y + o.camera.y,
+                  width: this.width,
+                  height: this.height,
+              },
+              offsetLeft: o.offsetLeft,
+              offsetTop: o.offsetTop,
+              view: o.view,
+          });
+      }
+
+      this.update(o);
+      this.physics.update();
+
+      if (this.size.autoAdjust) {
+          this.size.width = this.width;
+          this.size.height = this.height;
+      }
+      if (isFinite(this.degrees)) {
+          while (this.degrees >= 360) {
+              this.degrees -= 360;
+          }
+    
+          while (this.degrees < 0) {
+            this.degrees += 360;
+          }
+      }
+
+      for (i = 0; i < 10; i++) {
+          var pointer = HYPER.Input.Pointer.getPointer(i), b;
+          this._pointerX = ((pointer.x - o.offsetLeft) * (o.camera.width / o.view.width)) - o.camera.x;
+          this._pointerY = ((pointer.y - o.offsetTop) * (o.camera.height / o.view.height)) - o.camera.y;
+          if (this._pointerX >= this.x + this.size.x &&
+              this._pointerX <= this.x + this.size.width &&
+              this._pointerY >= this.y + this.size.y &&
+              this._pointerY <= this.y + this.size.height) {
+                this._InputInfo = {
+                    x: this._pointerX,
+                    y: this._pointerY
                 };
+                for (b = 0; b < this.events.onhover.length; b++) {
+                    this.events.onhover[b](this._InputInfo);
+                    this.onhover(this._InputInfo);
+                }
+                if (pointer.down) {
+                    for (b = 0; b < this.events.ondown.length; b++) {
+                        this.events.ondown[b](this._InputInfo);
+                        this.ondown(this._InputInfo);
+                    }
+                }
+                if (pointer.up) {
+                    for (b = 0; b < this.events.onup.length; b++) {
+                        this.events.onup[b](this._InputInfo);
+                        this.onup(this._InputInfo);
+                    }
+                    for (b = 0; b < this.events.onclick.length; b++) {
+                        this.events.onclick[b](this._InputInfo);
+                        this.onclick(this._InputInfo);
+                    }
+                }
+                if (pointer.hold) {
+                    for (b = 0; b < this.events.onhold.length; b++) {
+                        this.events.onhold[b](this._InputInfo);
+                        this.onhold(this._InputInfo);
+                    }
+                }
+                if (pointer.dblclick) {
+                    for (b = 0; b < this.events.ondblclick.length; b++) {
+                        this.events.ondblclick[b](this._InputInfo);
+                        this.ondblclick(this._InputInfo);
+                    }
+                }
             }
         }
     };
+
 })();
